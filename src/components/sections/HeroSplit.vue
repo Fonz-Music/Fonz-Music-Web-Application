4a71adfb9eb7b7787a65876b7e6d59b78f67f272
ben-vaughan <56156727+ben-vaughan@users.noreply.github.com>
2020-09-08 21:48:30 +0100

    Changed the title of the homepage.

--- src/components/sections/HeroSplit.vue
+++ src/components/sections/HeroSplit.vue
@@ -24,7 +24,7 @@
                     <div class="split-item">
                         <div class="hero-content split-item-content center-content-mobile">
                             <h1 class="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="150">
-                                Landing template for startups
+                                Fonz Music is the Best
                             </h1>
                             <p class="mt-0 mb-32 reveal-from-bottom" data-reveal-delay="300">
                                 Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.

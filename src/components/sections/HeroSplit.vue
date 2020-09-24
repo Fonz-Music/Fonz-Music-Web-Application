1a16b7fb7282fbffa965685692581e5686ffd16e
Dermot O'Brien <contact@dermotobrien.me>
2020-09-24 15:21:55 +0100

    Update LP title

--- src/components/sections/HeroSplit.vue
+++ src/components/sections/HeroSplit.vue
@@ -1,64 +1,84 @@
 <template>
-    <section
-        class="hero section"
-        :class="[
+  <section
+    class="hero section"
+    :class="[
             topOuterDivider && 'has-top-divider',
             bottomOuterDivider && 'has-bottom-divider',
             hasBgColor && 'has-bg-color',
             invertColor && 'invert-color'
-        ]">
-        <div class="container">
-            <div
-                class="hero-inner section-inner"
-                :class="[
+        ]"
+  >
+    <div class="container">
+      <div
+        class="hero-inner section-inner"
+        :class="[
                     topDivider && 'has-top-divider',
                     bottomDivider && 'has-bottom-divider'
-                ]">
-                <div
-                    class="split-wrap"
-                    :class="[
+                ]"
+      >
+        <div
+          class="split-wrap"
+          :class="[
                         invertMobile && 'invert-mobile',
                         invertDesktop && 'invert-desktop',
                         alignTop && 'align-top'
-                    ]">
-                    <div class="split-item">
-                        <div class="hero-content split-item-content center-content-mobile">
-                            <h1 class="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="150">
-                                Fonz Music
-                            </h1>
-                            <p class="mt-0 mb-32 reveal-from-bottom" data-reveal-delay="300">
-                                queue the party.
-                            </p>
-                            <div class="reveal-from-bottom" data-reveal-delay="450">
-                                <c-button tag="a" color="primary" wide-mobile href="/buy">
-                                    pre-order now
-                                </c-button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    ]"
+        >
+          <div class="split-item">
+            <div class="hero-content split-item-content center-content-mobile reveal-from-top">
+              <h1 class="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="150">
+                Make music
+                <span style="color: #fcc485;">collaborative</span> again.
+              </h1>
+              <p class="mt-0 mb-32 reveal-from-bottom" data-reveal-delay="300">
+                Tap the coaster,
+                <b>Queue the party</b>
+              </p>
+              <div class="reveal-from-bottom" data-reveal-delay="450">
+                <c-button tag="a" color="primary" wide-mobile href="/buy">Get it now!</c-button>
+              </div>
             </div>
+            <div
+              class="hero-figure split-item-image split-item-image-fill illustration-element-01 reveal-from-bottom"
+            >
+              <figure>
+                <c-image
+                  class="image-larger"
+                  :src="require('@/assets/images/splashcoaster.png')"
+                  alt="Splash Coaster"
+                  :width="712"
+                  :height="400"
+                />
+              </figure>
+            </div>
+          </div>
         </div>
-    </section>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import { SectionSplitProps } from '@/utils/SectionProps.js'
-import CButton from '@/components/elements/Button.vue'
+import { SectionSplitProps } from "@/utils/SectionProps.js";
+import CButton from "@/components/elements/Button.vue";
+import CImage from "@/components/elements/Image.vue";
+import CButtonGroup from "@/components/elements/ButtonGroup.vue";
 
 export default {
-  name: 'CHeroSplit',
+  name: "CHeroSplit",
   components: {
-    CButton
+    CButton,
+    CImage,
+    CButtonGroup,
   },
-  mixins: [SectionSplitProps]
-}
+  mixins: [SectionSplitProps],
+};
 </script>
 
 <style scoped>
 @media (min-width: 641px) {
-  .split-wrap .split-item {
+  /* .split-wrap .split-item {
     min-height: 492px;
-  }
+  } */
 }
 </style>

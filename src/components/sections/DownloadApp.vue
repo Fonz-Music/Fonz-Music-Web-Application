4d9111a9dc3df15f74b600aa674728a3f3962519
Dermot O'Brien <contact@dermotobrien.me>
2020-09-30 15:57:47 +0100

    Create download app component template

---
+++ src/components/sections/DownloadApp.vue
@@ -0,0 +1,50 @@
+<template>
+    <section
+        class="cta section center-content-mobile reveal-from-bottom"
+        :class="[
+            topOuterDivider && 'has-top-divider',
+            bottomOuterDivider && 'has-bottom-divider',
+            hasBgColor && 'has-bg-color',
+            invertColor && 'invert-color'
+        ]">
+        <div class="container">
+            <div
+                class="cta-inner section-inner"
+                :class="[
+                    topDivider && 'has-top-divider',
+                    bottomDivider && 'has-bottom-divider',
+                    split && 'cta-split'
+                ]">
+                <div class="cta-slogan">
+                    <h3 class="m-0">
+                        Leo vel fringilla est integer malesuada.
+                    </h3>
+                </div>
+                <div class="cta-action">
+                    <c-button tag="a" wide-mobile href="#">
+                        Get started now
+                    </c-button>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+import { SectionProps } from '@/utils/SectionProps.js'
+import CButton from '@/components/elements/Button.vue'
+
+export default {
+  name: 'CCta',
+  components: {
+    CButton
+  },
+  mixins: [SectionProps],
+  props: {
+    split: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>

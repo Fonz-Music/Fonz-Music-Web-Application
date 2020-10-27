6a8422f6fcd1999fb3d11e5df08806b5357af5c7
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2020-10-27 19:48:55 -0400

    trying to add stripe

--- src/components/sections/checkoutForms/stripePaymentVue.vue
+++ src/components/sections/checkoutForms/stripePaymentVue.vue
@@ -1,49 +1,162 @@
 <template>
   <div>
-    <stripe-elements
-      ref="elementsRef"
-      :pk="publishableKey"
-      :amount="amount"
-      locale="de"
-      @token="tokenCreated"
-      @loading="loading = $event"
-    >
-    </stripe-elements>
-    <button @click="submit">Pay ${{ amount / 100 }}</button>
+    <form id="payment-form">
+      <slot name="card-element">
+        <div id="card-element"></div>
+      </slot>
+      <slot name="card-errors">
+        <div id="card-errors" role="alert"></div>
+      </slot>
+      <button ref="submitButtonRef" type="submit" class="hide"></button>
+    </form>
   </div>
 </template>
 
 <script>
-import { StripeElements } from "vue-stripe-checkout";
+// import { loadStripeSdk } from "./load-checkout";
 export default {
-  components: {
-    StripeElements
+  props: {
+    pk: {
+      type: String,
+      required: true
+    },
+    amount: {
+      type: Number
+    },
+    stripeAccount: {
+      type: String
+    },
+    apiVersion: {
+      type: String
+    },
+    locale: {
+      type: String,
+      default: "auto"
+    },
+    styleObject: {
+      type: Object
+    }
   },
-  data: () => ({
-    loading: false,
-    amount: 1000,
-    publishableKey: process.env.PUBLISHABLE_KEY,
-    token: null,
-    charge: null
-  }),
+  data() {
+    return {
+      loading: false,
+      stripe: null,
+      elements: null,
+      card: null,
+      STRIPE_JS_SDK_URL: "https://js.stripe.com"
+    };
+  },
+  computed: {
+    style() {
+      return {
+        base: {
+          color: "#32325d",
+          fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+          fontSmoothing: "antialiased",
+          fontSize: "16px",
+          "::placeholder": {
+            color: "#aab7c4"
+          }
+        },
+        invalid: {
+          color: "#fa755a",
+          iconColor: "#fa755a"
+        }
+      };
+    },
+    form() {
+      return document.getElementById("payment-form");
+    }
+  },
   methods: {
     submit() {
-      this.$refs.elementsRef.submit();
+      this.$refs.submitButtonRef.click();
     },
-    tokenCreated(token) {
-      this.token = token;
-      // for additional charge objects go to https://stripe.com/docs/api/charges/object
-      this.charge = {
-        source: token.id,
-        amount: this.amount, // the amount you want to charge the customer in cents. $100 is 1000 (it is strongly recommended you use a product id and quantity and get calculate this on the backend to avoid people manipulating the cost)
-        description: this.description // optional description that will show up on stripe when looking at payments
-      };
-      this.sendTokenToServer(this.charge);
-    },
-    sendTokenToServer(charge) {
-      // Send to charge to your backend server to be processed
-      // Documentation here: https://stripe.com/docs/api/charges/create
+    loadStripeSdk: function(pk, version = "v3", callback) {
+      if (window.Stripe) {
+        callback();
+        return;
+      }
+      let e = document.createElement("script");
+      e.src = `${STRIPE_JS_SDK_URL}/${version}`;
+      e.type = "text/javascript";
+      document.getElementsByTagName("head")[0].appendChild(e);
+      e.addEventListener("load", callback);
     }
+  },
+  mounted() {
+    loadStripeSdk(this.pk, "v3", () => {
+      const options = {
+        stripeAccount: this.stripeAccount,
+        apiVersion: this.apiVersion,
+        locale: this.locale
+      };
+      this.stripe = window.Stripe(this.pk, options);
+      this.elements = this.stripe.elements();
+      this.card = this.elements.create("card", {
+        style: this.styleObject || this.style
+      });
+      this.card.mount("#card-element");
+      this.card.addEventListener("change", ({ error }) => {
+        const displayError = document.getElementById("card-errors");
+        if (error) {
+          displayError.textContent = error.message;
+          return;
+        }
+        displayError.textContent = "";
+      });
+
+      this.form.addEventListener("submit", async event => {
+        try {
+          this.$emit("loading", true);
+          event.preventDefault();
+          const data = {
+            ...this.card
+          };
+          if (this.amount) data.amount = this.amount;
+          const { token, error } = await this.stripe.createToken(data);
+          if (error) {
+            const errorElement = document.getElementById("card-errors");
+            errorElement.textContent = error.message;
+            console.error(error);
+            this.$emit("error 1", error);
+            return;
+          }
+          this.$emit("token", token);
+        } catch (error) {
+          console.error(error);
+          this.$emit("error", error);
+        } finally {
+          this.$emit("loading", false);
+        }
+      });
+    });
   }
 };
 </script>
+
+<style scoped>
+.StripeElement {
+  box-sizing: border-box;
+  height: 40px;
+  padding: 10px 12px;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  background-color: white;
+  box-shadow: 0 1px 3px 0 #e6ebf1;
+  -webkit-transition: box-shadow 150ms ease;
+  transition: box-shadow 150ms ease;
+}
+.StripeElement--focus {
+  box-shadow: 0 1px 3px 0 #cfd7df;
+}
+.StripeElement--invalid {
+  border-color: #fa755a;
+}
+.StripeElement--webkit-autofill {
+  background-color: #fefde5 !important;
+}
+.hide {
+  visibility: hidden;
+}
+</style>

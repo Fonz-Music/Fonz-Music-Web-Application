9d3d3efc2a1f02576c6900dc702ef8b97f9986a0
Dermot O'Brien <contact@dermotobrien.me>
2020-11-08 17:54:21 +0000

    Minor bug fix

--- functions/routes/Cart.js
+++ functions/routes/Cart.js
@@ -29,16 +29,16 @@ router.get('/:cartId', (req, res) => {
 router.put('/:cartId/:email', (req, res) => {
     const { cartId, email } = req.params;
     Shop.addEmailToCart(cartId, email).then(() => {
-        res.json({ cartId, email }});
+        res.json({ cartId, email });
     }).catch((error) => {
         res.status(error.status || 500).send(error);
     })
-})
+});
 
 router.put("/:cartId", (req, res) => {
     const { cartId } = req.params;
     const { packageId, currency } = req.body;
-    if(!packageId || !currency || !email) res.status(401).json({ message: "Parameters missing." })
+    if(!packageId || !currency) res.status(401).json({ message: "Parameters missing." })
     Shop.updateCart(packageId, currency, cartId).then(() => {
         res.json({ packageId, currency, cartId });
     }).catch((error) => {

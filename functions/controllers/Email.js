23c2de2680f6d98040e5152efbebfa950f988612
Dermot O'Brien <contact@dermotobrien.me>
2020-11-09 18:31:44 +0000

    Email sending

--- functions/controllers/Email.js
+++ functions/controllers/Email.js
@@ -3,23 +3,33 @@ const nodemailer = require('nodemailer');
 
 const userEmail = 'contact@fonzmusic.com';
 const userPassword = 'BlackBird20$';
-const transporter = nodemailer.createTransport(`smtps://${userEmail}:${userPassword}@smtp.zoho.eu`);
+// const transporter = nodemailer.createTransport(`smtps://${userEmail}:${userPassword}@smtp.zoho.eu`);
+const transporter = nodemailer.createTransport({
+    host: 'smtp.zoho.com',
+    port: 465,
+    secure: true, // use SSL
+    auth: {
+        user: userEmail,
+        pass: userPassword
+    }
+});
 
 exports.sendEmail = (emailTo) => {
     return new Promise(async (resolve, reject) => {
         try {
             var mailOptions = {
-                from: userEmail,    // sender address
+                from: `"Fonz Music" <${userEmail}>`,    // sender address
                 to: emailTo, // list of receivers
-                subject: 'Thank you for your error', // Subject line
-                text: 'Hello world from Node.js',       // plaintext body
-                html: '<b>Hello world from Node.js</b>' // html body
+                subject: 'Thank you for your order', // Subject line
+                text: 'You have ordered 1 Fonz Coaster',       // plaintext body
+                html: 'You have ordered <b>1 Fonz Coaster</b>' // html body
             };
             transporter.sendMail(mailOptions, function(error, info){
                 if(error) return reject(error);
                 return resolve(info.response);
             });
         } catch(error) {
+            console.error(error);
             reject(error);
         }
     });

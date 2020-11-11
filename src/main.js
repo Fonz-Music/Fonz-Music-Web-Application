12dc2415c396ed5cb631cb939509941fc0d845a4
Dermot O'Brien <contact@dermotobrien.me>
2020-11-11 10:41:22 +0000

    IP location restructure

--- src/main.js
+++ src/main.js
@@ -6,17 +6,28 @@ Vue.use(Fragment.Plugin);
 import Vue2TouchEvents from "vue2-touch-events";
 Vue.use(Vue2TouchEvents);
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
-import { faGooglePlay, faAppStore } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  library
+} from "@fortawesome/fontawesome-svg-core";
+import {
+  faUserSecret
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faGooglePlay,
+  faAppStore
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  FontAwesomeIcon
+} from "@fortawesome/vue-fontawesome";
 library.add(faUserSecret);
 library.add(faGooglePlay, faAppStore);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 // import vuetify from './plugins/vuetify' // path to vuetify export
 // import Vuetify from 'vuetify/lib'
-import { BootstrapVue } from "bootstrap-vue";
+import {
+  BootstrapVue
+} from "bootstrap-vue";
 Vue.use(BootstrapVue);
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -30,10 +41,27 @@ import "@/assets/scss/style.scss";
 Vue.config.productionTip = false;
 // Vue.prototype.$API_URL = "http://localhost:5001/fonz-music-web/us-central1/app";
 Vue.prototype.$API_URL = "https://web.fonzmusic.com";
-Vue.prototype.currency = localStorage.getItem("currency") || "eur";
+// Vue.prototype.currency = localStorage.getItem("currency");
 // Vue.prototype.currencySymbol =;
 Vue.prototype.cartId = localStorage.getItem("cartId") || "";
 
+
+const axios = require('axios');
+let setCurrency = "eur";
+axios.get("https://ipgeolocation.abstractapi.com/v1/?api_key=eb598e256fe04910a25aba0bce726785").then((resp) => {
+  let country_code = resp.data.country_code;
+  if (country_code == "US") {
+    setCurrency = "usd";
+  } else if (country_code == "GB") {
+    setCurrency = "gbp"
+  }
+  // Vue.prototype.currency = setCurrency;
+  localStorage.setItem('currency', setCurrency)
+}).catch((error) => {
+  console.error("COULD NOT GET LOCATION DATA FOR PRICING ", error)
+})
+
+Vue.prototype.currency = localStorage.getItem('currency');
 new Vue({
   router,
   render: h => h(App)
@@ -52,34 +80,41 @@ Vue.use(VueGoogleMaps, {
   }
 });
 
-const https = require("https");
-https
-  .get(
-    "https://ipgeolocation.abstractapi.com/v1/?api_key=eb598e256fe04910a25aba0bce726785",
-    resp => {
-      let data = "";
-      // A chunk of data has been received.
-      resp.on("data", chunk => {
-        data += chunk;
-      });
 
-      // The whole response has been received. Print out the result.
-      resp.on("end", () => {
-        var userInfo = JSON.parse(data);
-        // console.log(userInfo.country_code);
-        if (userInfo.country_code == "US") {
-          this.currency = "usd";
-        } else if (userInfo.country_code == "UK") {
-          // console.log("going w gbp ");
-          this.currency = "gbp";
-        } else {
-          // console.log("going w eur ");
-          this.currency = "eur";
-        }
-      });
-    }
-  )
-  .on("error", err => {
-    this.currency = "eur";
-    // console.log("Error: " + err.message);
-  });
+
+// const https = require("https");
+// https
+//   .get(
+//     "https://ipgeolocation.abstractapi.com/v1/?api_key=eb598e256fe04910a25aba0bce726785",
+//     resp => {
+//       let data = "";
+//       // A chunk of data has been received.
+//       resp.on("data", chunk => {
+//         data += chunk;
+//       });
+
+//       // The whole response has been received. Print out the result.
+//       resp.on("end", () => {
+//         let setCurrency = "eur";
+//         var userInfo = JSON.parse(data);
+//         // console.log(userInfo.country_code);
+//         if (userInfo.country_code == "US") {
+//           setCurrency = "usd";
+//         } else if (userInfo.country_code == "GB") {
+//           // console.log("going w gbp ");
+//           setCurrency = "gbp";
+//         // } else {
+//         //   // console.log("going w eur ");
+//           // setCurrency = "EUR";
+//         }
+//         // Vue.prototype.currency = localStorage.getItem("currency") || "eur";
+//         console.log(setCurrency, " this currency")
+//         Vue.prototype.currency = setCurrency;
+//         localStorage.setItem("currency", setCurrency);
+//       });
+//     }
+//   )
+//   .on("error", err => {
+//     this.currency = "eur";
+//     // console.log("Error: " + err.message);
+//   });

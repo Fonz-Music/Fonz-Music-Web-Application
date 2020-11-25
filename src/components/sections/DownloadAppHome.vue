740c1fde522846e9a829f77f6eba39fa2eeb5710
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2020-11-24 23:07:48 -0500

    working on app carousel

--- src/components/sections/DownloadAppHome.vue
+++ src/components/sections/DownloadAppHome.vue
@@ -21,19 +21,49 @@
           </div>
         </div>
       </div>
-      // image
-      <div class="app-screenshot"></div>
+      <!-- image -->
+      <!-- <div class="app-screenshot">
+        <c-carousel :active="0">
+          <c-carousel-item
+            ><c-image
+              class="image-larger"
+              :src="require('@/assets/images/splashGraphic.png')"
+              alt="Splash Coaster"
+              :width="712"
+              :height="400"
+          /></c-carousel-item>
+          <c-carousel-item
+            ><c-image
+              class="logo"
+              :src="require('@/assets/images/logo.svg')"
+              alt="Neon"
+              :width="64"
+              :height="64"
+          /></c-carousel-item>
+        </c-carousel>
+      </div> -->
     </div>
   </section>
 </template>
 
 <script>
-export default {};
+import CCarousel from "@/components/elements/Carousel.vue";
+import CButton from "@/components/elements/Button.vue";
+import CCarouselItem from "@/components/elements/CarouselItem.vue";
+export default {
+  components: {
+    CCarousel,
+    CCarouselItem,
+    CButton
+  }
+};
 </script>
 
 <style lang="css" scoped>
 .download-the-app {
   max-width: 900px;
   margin: 50px auto;
+}
+.app-screenshot {
 }
 </style>

0bae779be175df9ee7aa038126958d32a32849d5
Dermot O'Brien <contact@dermotobrien.me>
2020-12-02 10:14:13 +0000

    Admin cleanup

--- src/components/sections/Admin/OrdersTable.vue
+++ src/components/sections/Admin/OrdersTable.vue
@@ -1,12 +1,10 @@
 <template>
-  <section class="table-style">
-
-    <!-- Date | Name | Address-->
-
-    <b-table hover :items="items"></b-table>
-
-    <p> {{ randOrderId }} </p>
-  </section>
+  <fragment>
+    <div class="table-style">
+      <b-table hover :items="items">
+      </b-table>
+    </div>
+  </fragment>
 </template>
 
 <script>
@@ -14,41 +12,112 @@ const db = firebase.firestore();
 
 export default {
   name: "COrdersTable",
+
   data() {
     return {
       items: [],
-      randOrderId: 1234
     };
   },
+
   methods: {
+
     async getOrders() {
+
+      // assign reference to database collection 'orders'
       const ordersRef = db.collection("orders");
-      const orders = await ordersRef
-        .where("fulfilled", "==", false)
-        .limit(30)
+      
+      // create object orders from ordersRef
+      const orders = await ordersRef 
+        .limit(10)
         .get();
 
+      // for each 'doc' in orders
       orders.forEach((doc) => {
+        const {created, fulfilled, stripe, cart, assignedTo} = doc.data();
+        var buyerName = 'TEST';
+        var date = 0;
+        var quantity = 0;
+        var value = 0;
+        var fufillment = false;
+        var address = "";
 
-        // const orderId = doc.id
+        // Check Name
+        if(stripe.billing_details.name != null) {
+          buyerName = stripe.billing_details.name;
+        }
+        else if(stripe.shipping != null) {
+          if(stripe.shipping.name != null) {
+            buyerName = stripe.shipping.name;
+          }
+        }
 
-        const orderId = doc.id;
-        const { fulfilled, assignedTo, cart, stripe } = doc.data();
-        const { quantity: coasterQuantity } = cart;
+        // Check Address
+        // const { city, country, line1, line2, postal_code, state } = stripe.billing_details.address;
+        // const address = line1 + " " + line2 + " " + city + " " + state + " " + country + " " + postal_code;
 
-        const name = stripe.billing_details.name;
-        const { city, country, line1, line2, postal_code, state } = stripe.billing_details.address;
-        const address = line1 + " " + line2 + " " + city + " " + state + " " + country + " " + postal_code;
+        if(stripe.billing_details.address.country != null) {
+          const { city, country, line1, line2, postal_code, state } = stripe.billing_details.address
+          address = country + " " + state + " " + postal_code + " " + city + " " + line1 + " " + line2;
+        }
 
-        console.log({ address })
+        else if(stripe.shipping != null) {
+          if(stripe.shipping.address != null) {
+            if(stripe.shipping.address.country != null) {
+              address += stripe.shipping.address.country;
+            }
+            if(stripe.shipping.address.state != null) {
+              address += stripe.shipping.address.state;
+            }
+            if(stripe.shipping.address.postal_code != null) {
+              address += stripe.shipping.address.postal_code;
+            }
+            if(stripe.shipping.address.city != null) {
+              address += stripe.shipping.address.city;
+            }
+            if(stripe.shipping.address.line1 != null) {
+              address += stripe.shipping.address.line1;
+            }
+            if(stripe.shipping.address.line2 != null) {
+              address += stripe.shipping.address.line2;
+            }
+          }
+        }
+        
+        // Check Date
+        if(created != null) {
+          const timeCreated = created.toDate();
+          date = timeCreated.getDate() + "/" + timeCreated.getMonth() + '/' + timeCreated.getFullYear();
+        }
 
-        // view more button, opens modal with more information
-        // doc/stripe/billing_details/address/name
+        // Check Cart
+        if(cart != null) {
+          quantity = cart.quantity;
+          value = '$' + cart.price;
+        }
 
-        this.items.push({address});
+        // Check Fulfillment
+        if(fulfilled != null) {
+          fufillment = fulfilled;
+        }
+
+        this.items.push({date, buyerName, quantity, value, address, fufillment});
+
+        // const { fulfilled, assignedTo, cart, stripe } = doc.data();
+        // const { quantity: coasterQuantity } = cart;
+
+        // const name = stripe.billing_details.name;
+        // const address = line1 + " " + line2 + " " + city + " " + state + " " + country + " " + postal_code;
+
+        // console.log({ address })
+
+        // // view more button, opens modal with more information
+        // // doc/stripe/billing_details/address/name
+        
+        // this.items.push({name, address});
       });
     },
   },
+
   mounted() {
     this.getOrders();
   },
@@ -59,5 +128,6 @@ export default {
 <style>
 .table-style {
   font-size: 5px;
+  padding: 5px 20px 5px 20px;
 }
 </style>

724b2d1a4eee12bcd49e8bd070dc346e2fc64679
ben-vaughan <56156727+ben-vaughan@users.noreply.github.com>
2021-01-26 17:05:56 +0000

    Minor bug fixes

--- src/views/Affiliate/AffiliateLogin.vue
+++ src/views/Affiliate/AffiliateLogin.vue
@@ -7,19 +7,16 @@
 
 
 <script>
-
-
 import CLayout from "@/layouts/LayoutDefault.vue";
 import CLoginSection from "@/components/sections/Affiliate/Login/LoginSection.vue";
-
 export default {
     name:'AffiliateLogin',
-
     components: {
         CLoginSection
     },
 
     created() {
         this.$emit("update:layout", CLayout);
-    },
+    }
 }
+</script>

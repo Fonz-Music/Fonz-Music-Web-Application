7d9e5452ae23bc41a59c83df701fb84927cdfbd7
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2021-02-23 15:28:20 -0500

    added msg saying "$5 off" to the affiliate login webpage

--- src/components/sections/Affiliate/Splash/AffiliateInfo.vue
+++ src/components/sections/Affiliate/Splash/AffiliateInfo.vue
@@ -15,6 +15,16 @@
               </div>
             </div>
           </div>
+          <div class="col-12 why-use-code">
+            <div class="row justify-content-center">
+              <span style="font-weight:bold; font-size: 30px; color:grey;"> {{determineCurrencySymbol}}5 Off Their Order  </span>
+            </div>
+            <div class="row justify-content-center">
+              <div class="col-8" id="paragraph-style">
+                <span> Why Use Your Code? When your friends, family, or others purchase a coaster with your promo code, they save {{determineCurrencySymbol}}5 on their order! Everyone wins. </span>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="row">
         </div>
@@ -26,7 +36,17 @@
 <script>
 export default {
   name: "AffiliateInfo",
-
+  computed: {
+    determineCurrencySymbol() {
+      if (this.currency == null) {
+        console.log("grabbing currency");
+      }
+      console.log("this cur " + this.currency);
+      if (this.currency == "usd") return "$";
+      else if (this.currency == "gbp") return "£";
+      else return "€";
+    }
+  },
 }
 </script>
 
@@ -36,5 +56,8 @@ export default {
     padding-top: 10px;
     color: grey;
     font-size: 18px;
+  }
+  .why-use-code {
+    padding-top: 50px
   }
 </style>

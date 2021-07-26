84c0bb028e4b4edf57ef2feff632f6d4932da555
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2021-07-26 11:03:41 +0100

    updated router to have all file paths uppercase

--- src/router.js
+++ src/router.js
@@ -2,34 +2,34 @@ import Vue from "vue";
 import Router from "vue-router";
 
 // Main
-import Home from "./views/main/Home.vue";
-import Buy from "./views/main/Buy.vue";
-import About from "./views/main/About.vue";
-import Team from "./views/main/Team.vue";
-import Contact from "./views/main/Contact.vue";
+import Home from "./views/Main/Home.vue";
+import Buy from "./views/Main/Buy.vue";
+import About from "./views/Main/About.vue";
+import Team from "./views/Main/Team.vue";
+import Contact from "./views/Main/Contact.vue";
 
 // eCommerce
-import CheckoutPage from "./views/ecommerce/CheckoutPage.vue";
-import PayWithCreditCard from "./views/ecommerce/PayWithCreditCard.vue";
-import OrderSuccess from "./views/ecommerce/OrderSuccess.vue";
-import OrderFailurePage from "./views/ecommerce/OrderFailurePage.vue";
-import PaymentPage from "./views/ecommerce/PaymentPage.vue";
+import CheckoutPage from "./views/eCommerce/CheckoutPage.vue";
+import PayWithCreditCard from "./views/eCommerce/PayWithCreditCard.vue";
+import OrderSuccess from "./views/eCommerce/OrderSuccess.vue";
+import OrderFailurePage from "./views/eCommerce/OrderFailurePage.vue";
+import PaymentPage from "./views/eCommerce/PaymentPage.vue";
 
 // Misc
-import PrivacyPolicy from "./views/misc/PrivacyPolicy.vue";
-import TermsOfService from "./views/misc/TermsOfService.vue";
-import PageNotFoundPage from "./views/misc/404notFound.vue";
+import PrivacyPolicy from "./views/Misc/PrivacyPolicy.vue";
+import TermsOfService from "./views/Misc/TermsOfService.vue";
+import PageNotFoundPage from "./views/Misc/404notFound.vue";
 import PromoPage from "./views/misc/promo.vue";
 
 // Affiliate
-import AffiliateLogin from "./views/affiliate/Affiliate.vue";
-import AffiliateDashboard from "./views/affiliate/AffiliateDashboard.vue";
-import AffiliateTransactions from "./views/affiliate/AffiliateTransactions.vue";
-import AffiliateAccount from "./views/affiliate/AffiliateAccount.vue";
+import AffiliateLogin from "./views/Affiliate/Affiliate.vue";
+import AffiliateDashboard from "./views/Affiliate/AffiliateDashboard.vue";
+import AffiliateTransactions from "./views/Affiliate/AffiliateTransactions.vue";
+import AffiliateAccount from "./views/Affiliate/AffiliateAccount.vue";
 
 // Admin
-import Admin from "./views/admin/Admin.vue";
-import AdminLogin from "./views/admin/AdminLogin.vue";
+import Admin from "./views/Admin/Admin.vue";
+import AdminLogin from "./views/Admin/AdminLogin.vue";
 
 Vue.use(Router);
 
